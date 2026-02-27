00383c9480aab1b2ea74150cdf8fe501696cabe9
gbLee <kblee@ispark.kr>
2026-02-27 14:25:24 +0900

    api health 체크 삭제

--- types/excel.ts
+++ types/excel.ts
@@ -1,13 +1,1 @@
 import type { Ref } from 'vue'
-
-export interface HealthResponse {
-  status?: string
-  message?: string
-  [key: string]: unknown
-}
-
-export interface HealthState {
-  healthData: Ref<string>
-  healthLoading: Ref<boolean>
-  healthError: Ref<string>
-}
