86e2d3b188c2e846503c8382edaeaab6af247f1c
김현지 <hji0530kim@gmail.com>
2026-03-03 14:28:25 +0900

    style: 아이콘 및 SCSS 믹스인 개선
    1. .icon 클래스에 flex-shrink 속성 추가 및 스타일 정리.
    2. 새로운 아이콘 클래스 (.icon-knowledge, .icon-database, .icon-sparkle, .icon-send) 추가 및 SCSS 믹스인 활용.
    3. 홈 페이지 채팅 인덱스에서 아이콘 사용 방식 개선.
    4. SCSS 믹스인에서 아이콘 관련 스타일 통합 및 정리.

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -7,27 +7,35 @@
 
     <div class="chat-index-input-wrapper flex flex-col items-center w-full">
       <div class="chat-index-input-top flex items-start w-full">
-        <i class="icon icon-sparkle"></i>
-        <textarea class="inp inp-chat-search" type="text" placeholder="궁금하신 내용을 입력하세요." />
+        <i class="icon icon-sparkle size-24"></i>
+        <textarea
+          class="inp inp-chat-search"
+          type="text"
+          placeholder="궁금하신 내용을 입력하세요."
+        />
       </div>
 
       <div class="chat-index-input-bottom flex justify-end items-center w-full">
-        <select class="inp inp-select inp-select-ai-model" name="ai-model" id="ai-model">
+        <select
+          id="ai-model"
+          class="inp inp-select inp-select-ai-model"
+          name="ai-model"
+        >
           <option value="1">자동</option>
           <option value="2">2</option>
           <option value="3">3</option>
         </select>
-        <button class="btn btn-chat-send"><i class="icon icon-send"></i></button>
+        <button class="btn btn-chat-send"><i class="icon icon-send size-20"></i></button>
       </div>
     </div>
 
     <div class="btn-grp">
       <button class="btn btn-chat-index">
-        <span class="icon-circle"><i class="icon-knowledge"></i></span>
+        <span class="icon-circle"><i class="icon icon-knowledge size-20"></i></span>
         <p>지식검색 (매뉴얼 AI)</p>
       </button>
       <button class="btn btn-chat-index">
-        <span class="icon-circle"><i class="icon-database"></i></span>
+        <span class="icon-circle"><i class="icon icon-database size-20"></i></span>
         <p>데이터분석 (SQL)</p>
       </button>
     </div>
@@ -35,9 +43,3 @@
 </template>
 
 <script setup lang="ts"></script>
-
-<style lang="scss" scoped>
-.chat-index {
-  padding: $spacing-xl;
-}
-</style>
