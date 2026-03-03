e05da150bc16930225c91f936f1943b3844aafc0
김현지 <hji0530kim@gmail.com>
2026-03-03 13:36:39 +0900

    style: SCSS 및 Vue 컴포넌트 개선
    1. 새로운 버튼 스타일 추가: .btn-chat-index 및 .btn-chat-send 클래스 정의.
    2. 입력 필드 스타일 개선: .inp-select 클래스 추가 및 focus 효과 정의.
    3. 아이콘 클래스 이름 변경: .icon-manual → .icon-knowledge, .icon-data → .icon-database, 새로운 아이콘 추가.
    4. 홈 페이지 채팅 인덱스 레이아웃 및 스타일 업데이트: 입력 필드 및 버튼 그룹 개선.

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -5,21 +5,31 @@
       <p class="chat-index-description f-center">어떤게 궁금하세요?</p>
     </div>
 
-    <div class="chat-index-content flex flex-col items-center w-full">
-      <div class="chat-index-input-wrapper">
-        <input type="text" placeholder="질문을 입력해주세요." />
+    <div class="chat-index-input-wrapper flex flex-col items-center w-full">
+      <div class="chat-index-input-top flex items-start w-full">
+        <i class="icon icon-sparkle"></i>
+        <input class="inp-chat-search" type="text" placeholder="궁금하신 내용을 입력하세요." />
       </div>
 
-      <div class="btn-grp">
-        <button class="btn btn-chat-index">
-          <span class="icon-circle"><i class="icon-manual"></i></span>
-          <p>지식검색 (매뉴얼 AI)</p>
-        </button>
-        <button class="btn btn-chat-index">
-          <span class="icon-circle"><i class="icon-data"></i></span>
-          <p>데이터분석 (SQL)</p>
-        </button>
+      <div class="chat-index-input-bottom flex justify-end items-center w-full">
+        <select class="inp-select inp-select-ai-model" name="ai-model" id="ai-model">
+          <option value="1">자동</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <button class="btn btn-chat-send"><i class="icon icon-send"></i></button>
       </div>
+    </div>
+
+    <div class="btn-grp">
+      <button class="btn btn-chat-index">
+        <span class="icon-circle"><i class="icon-knowledge"></i></span>
+        <p>지식검색 (매뉴얼 AI)</p>
+      </button>
+      <button class="btn btn-chat-index">
+        <span class="icon-circle"><i class="icon-database"></i></span>
+        <p>데이터분석 (SQL)</p>
+      </button>
     </div>
   </div>
 </template>
